c0d66c1166143640aeba2b3ce32107629bab77e8
Sarah <sarahrebecca.meyer@students.fhnw.ch>
2024-03-12 20:06:52 +0100

    fixed test_script

--- 2_GC/test_script_2.rb
+++ 2_GC/test_script_2.rb
@@ -1,41 +1,97 @@
 require 'minitest/autorun'
 require 'minitest/mock'
+require 'bio'
 require_relative 'exercise_2.rb' 
 
 
-class TestValidateFastaFile < Minitest::Test
-  def setup
-    @file_path = ['test_human_gene.fna','test_human_gene_2.fna']  # Path to a 2 test files (1 invalid header, 1 invalid sequence) 
-  end
-  def test_validate_fasta_file_raises_exception
-    assert_raises(StandardError) do
-      @file_path.each do |file_path|
-        validate_fasta_file!(file_path)
-      end
+class TestCalculateMD5 < Minitest::Test
+  
+  class Exercise1Test < Minitest::Test
+    def test_check_filename_with_no_arguments
+      ARGV.clear
+      assert_output("Please enter it in the format like this: ruby exercise_1.rb FILENAME\n") { exit_assertion { check_filename } }
+    end
+  
+    def test_check_filename_with_multiple_arguments
+      ARGV.replace(['file1.fna', 'file2.fna'])
+      assert_output("Please enter it in the format like this: ruby exercise_1.rb FILENAME\n") { exit_assertion { check_filename } }
+    end
+  
+    def test_check_filename_with_nonexistent_file
+      ARGV.replace(['nonexistent.fna'])
+      assert_output("File does not exist or is not readable.\n") { exit_assertion { check_filename } }
+    end
+  
+    def test_check_filename_with_valid_file
+      file_path = 'valid_file.fna'
+      File.write(file_path, ">Sample\nATCG")
+      ARGV.replace([file_path])
+      assert_equal file_path, check_filename
+    ensure
+      File.delete(file_path) if File.exist?(file_path)
+    end
+  
+    private
+  
+    def exit_assertion
+      yield
+    rescue SystemExit => e
+      assert true  # If we get here, it means exit was called, which is expected in some tests
+    else
+      assert false, "Expected script to exit but it didn't"
     end
   end
+  
 end
 
-class TestProcessFile < Minitest::Test
-  def setup
-    @original_stdout = $stdout
-    $stdout = StringIO.new
-    @file_path = 'gene.fna' #testing with the mouse Gene CD28 
+class Exercise1Test < Minitest::Test
+  # Helper method to create a temporary FASTA file
+  def create_temp_fasta(content)
+    file_path = "temp.fasta"
+    File.open(file_path, "w") do |file|
+      file.write(content)
+    end
+    file_path
   end
 
-  def teardown
-    $stdout = @original_stdout
+  def test_validate_fasta_file_with_valid_entries
+    content = ">Valid_1\nATCG\n>Valid_2\nGCTA"
+    file_path = create_temp_fasta(content)
+    valid_entries, invalid_entries = validate_fasta_file!(file_path)
+    assert_equal 2, valid_entries.size
+    assert_equal 0, invalid_entries.size
+  ensure
+    File.delete(file_path) if File.exist?(file_path)
   end
 
-  def test_process_file
-    process_file(@file_path)
-    expected_output = <<~OUTPUT.chomp
-      Entry ID: NC_000067.7:60785547-60812521
-      GC Content Percentage: 39.31%
-  
-      Total entries: 1
-    OUTPUT
-  
-    assert_equal expected_output, $stdout.string.chomp, "Output did not match expected"
-  end  
+  def test_validate_fasta_file_with_invalid_entry_ids
+    content = ">\nATCG\n>Valid_2\nGCTA"
+    file_path = create_temp_fasta(content)
+    valid_entries, invalid_entries = validate_fasta_file!(file_path)
+    assert_equal 1, valid_entries.size
+    assert_equal 1, invalid_entries.size
+  ensure
+    File.delete(file_path) if File.exist?(file_path)
+  end
+
+  def test_validate_fasta_file_with_empty_sequences
+    content = ">Valid_1\n\n>Valid_2\nGCTA"
+    file_path = create_temp_fasta(content)
+    valid_entries, invalid_entries = validate_fasta_file!(file_path)
+    assert_equal 1, valid_entries.size
+    assert_equal 1, invalid_entries.size
+  ensure
+    File.delete(file_path) if File.exist?(file_path)
+  end
+
+  def test_validate_fasta_file_with_mixed_validity_entries
+    content = ">\n\n>Valid_2\nGCTA\n>Valid_3\nATCG\n>Invalid_3\n"
+    file_path = create_temp_fasta(content)
+    valid_entries, invalid_entries = validate_fasta_file!(file_path)
+    assert_equal 2, valid_entries.size
+    assert_equal 2, invalid_entries.size
+  ensure
+    File.delete(file_path) if File.exist?(file_path)
+  end
 end
+
