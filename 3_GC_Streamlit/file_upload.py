86c1e407dfd7d7f1d14754a749804eccffb6f9ce
Sarah <sarahrebecca.meyer@students.fhnw.ch>
2024-03-20 19:29:22 +0100

    new fasta_file_upload function

---
+++ 3_GC_Streamlit/file_upload.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+from Bio import SeqIO
+from fasta_class import FastaClass
+# from Bio.SeqUtils import GC
+
+all_entries = []
+
+def fasta_file_upload():
+    uploaded_file = st.file_uploader("Upload your FASTA file", type=['fasta', 'fna'])
+
+    if uploaded_file is not None:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+
+        for record in SeqIO.parse(stringio, "fasta"):
+            fasta = FastaClass(record.id, record.seq)
+            all_entries.append(fasta)
+
+    return all_entries
+
+
