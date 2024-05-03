77119a03b70a4731f5e73f8b8940081a726b2bc7
Sarah <sarahrebecca.meyer@students.fhnw.ch>
2024-05-03 11:07:23 +0200

    refactoring SequenceStorage

--- Exercise 6/dna2protein.py
+++ Exercise 6/dna2protein.py
@@ -8,14 +8,26 @@ class DNASequenceTranslator:
     @staticmethod
     def transcribe_dna_to_rna(dna):
         result = dna.transcribe()
-        return result
-
+        storage.save('RNA', result)
+        return result, storage
+        
     @staticmethod
     def translate_rna_to_protein(rna):
         result = rna.translate()
-        return result
+        storage.save('Protein', result)
+        return storage
     
-class SequenceStorage:
+    
+# class SequenceStorage(ABC):
+#     #this holds the instance that will be created
+
+#     def save(self, name, seq):
+#         pass
+
+#     def read(self, name):
+#         pass
+    
+class SequenceStorage():
     #this holds the instance that will be created
     _instance = None
 
@@ -30,7 +42,8 @@ class SequenceStorage:
 
     def read(self, name):
         return self.data[name]
-    
+
+
 class SequenceGenerator(ABC):
     #this is an abstract base class so the individual calsses can make use of polymorphism
     @abstractmethod
@@ -69,7 +82,9 @@ def output():
     print("Original Sequences: ")
     print("DNA Sequence:", storage.read('DNA'))
     print("RNA Sequence:", rna_seq_obj)
-    print("Protein Sequence:", protein_seq)
+    #print("Protein Sequence:", protein_seq)
+
+
 
 def main():
     output()
@@ -77,18 +92,22 @@ def main():
 if __name__ == '__main__':
 
     sequence = 'GTGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG'
-
+    storage = SequenceStorage()
+    storage.save('DNA', sequence)
     #creating a Seq onject for the sequence
     dna_seq_obj = Seq(sequence)
 
-    rna_seq_obj = DNASequenceTranslator.transcribe_dna_to_rna(dna_seq_obj)
+    rna_seq_obj, storage = DNASequenceTranslator.transcribe_dna_to_rna(dna_seq_obj)
     
     # RNA to protein sequence
-    protein_seq = DNASequenceTranslator.translate_rna_to_protein(rna_seq_obj)
-
+    stored_protein = DNASequenceTranslator.translate_rna_to_protein(rna_seq_obj)
+    print("Protein from storage:", stored_protein.read("Protein"))
     # Create a new object of the SequenceStorage class
-    storage = SequenceStorage()
-    storage.save('DNA', sequence)
+    # storage = SequenceStorage()
+    # storage.save('DNA', sequence)
+    print(storage.read('DNA'))
+    print(storage.read('RNA'))
+    print(storage.read('Protein'))
 
     # Creating a random Sequence with DNASequenceGeneratir
     dna_generator = DNASequenceGenerator()
