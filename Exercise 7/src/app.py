2d8e529bc977a4321e0d2f8dfd46e6dcdeb4a4bc
Sarah <sarahrebecca.meyer@students.fhnw.ch>
2024-05-20 17:01:22 +0200

    fixed app.py with pylint to get 9.48

--- Exercise 7/src/app.py
+++ Exercise 7/src/app.py
@@ -1,87 +1,103 @@
-import datastorage
-import tracemalloc
-import psutil
-import os
-import logging
+""" Main application file for the data collection service exercise"""
+# import os
+# import logging
+# import tracemalloc
+# import psutil
+
 
 import json
 from flask import request, Flask
+import datastorage
+
 
 app = Flask(__name__)
 
 class Experiment:
-  def __init__(self, id, name):
-    self.id = id
-    self.name = name
+    """Experiment class"""
+    def __init__(self, record_id, name):
+        self.record_id = record_id
+        self.name = name
+
 
 def to_json(obj):
-  return json.dumps(obj, default=lambda obj: obj.__dict__)
+    """Convert object to json format"""
+    return json.dumps(obj, default=lambda obj: obj.__dict__)
 
 @app.route('/info', methods=['GET'])
 def index():
+    """Return information about the application"""
+
     return json.dumps({'application' : 'Data Collection Service',
-    'author' : 'David Herzig', 'version' : 'v1_0_0', 'Support' : 'dave.herzig@gmail.com'})
+    'author' : 'Davrecord_id Herzig', 'version' : 'v1_0_0', 
+    'Support' : 'dave.herzig@gmail.com', 'adjusted by' : 'Sarah Rebecca Meyer'})
 
 @app.route('/experiment', methods=['POST', 'GET'])
+
 def experiment_action():
-  ds = datastorage.DataStorage()
-  if request.method == 'POST':
-    form_data = request.data
-    data = json.loads(form_data)
-    id = ds.create_experiment(data['name'])
-    return json.dumps({'result' : id})
-  if request.method == 'GET':
-    exps = ds.get_experiments()
-    exps_array = []
-    for key, value in exps.items():
-      exp = Experiment(key, value)
-      exps_array.append(exp)
-    return to_json(exps_array)
-  
+    """Create a new experiment or get all experiments"""
+
+    data_storage = datastorage.DataStorage()
+    if request.method == 'POST':
+        form_data = request.data
+        data = json.loads(form_data)
+        record_id = data_storage.create_experiment(data['name'])
+        return json.dumps({'result' : record_id})
+    if request.method == 'GET':
+        exps = data_storage.get_experiments()
+        exps_array = []
+        for key, value in exps.items():
+            exp = Experiment(key, value)
+            exps_array.append(exp)
+        return to_json(exps_array)
+
 @app.route('/patient', methods=['POST', 'GET'])
 def patient_action():
-  ds = datastorage.DataStorage()
-  if request.method == 'POST':
-    form_data = request.data
-    data = json.loads(form_data)
-    id = ds.create_patient(data['name'])
-    return json.dumps({'result' : id})
-  if request.method == 'GET':
-    return ds.get_patients()
-  
+    """Create a new patient or get all patients"""
+    data_storage = datastorage.DataStorage()
+    if request.method == 'POST':
+        form_data = request.data
+        data = json.loads(form_data)
+        record_id = data_storage.create_patient(data['name'])
+        return json.dumps({'result' : record_id})
+    if request.method == 'GET':
+        return data_storage.get_patients()
+
+
 @app.route('/store', methods=['POST'])
 def store_data():
-  ds = datastorage.DataStorage()
-  form_data = request.data
-  data = json.loads(form_data)
-  filename = data['filename']
-  type = data['type']
-  
-  if type == 'experiments':
-    ds.store_experiments(filename)
-  elif type == 'patients':
-    ds.store_patients(filename)
-  elif type == 'data':
-    ds.store_data(filename)
-  else:
-    print('invalid data type: ' + type)
-    
-  return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+    """Store data in the data storage"""
+    data_storage = datastorage.DataStorage()
+    form_data = request.data
+    data = json.loads(form_data)
+    filename = data['filename']
+    data_type = data['type']
 
-  
+
+    if data_type == 'experiments':
+        data_storage.store_experiments(filename)
+    elif data_type == 'patients':
+        data_storage.store_patients(filename)
+    elif data_type == 'data':
+        data_storage.store_data(filename)
+    else:
+        print('invalrecord_id data type: ' + data_type)
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+
 @app.route('/data', methods=['POST'])
 def upload_data():
-  form_data = request.data
-  print(form_data)
-  data = json.loads(form_data)
-  ds = datastorage.DataStorage()
-  ds.add_data(data)
+    """Upload data to the data storage"""
+    form_data = request.data
+    print(form_data)
+    data = json.loads(form_data)
+    data_storage = datastorage.DataStorage()
+    data_storage.add_data(data)
 
-  return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
-  
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
 
 if __name__ == '__main__':
 
-  app.run(debug=True, host="0.0.0.0", port=8080)
-  #app.run(debug=True, port=8080)
-  
+    app.run(debug=True, host="0.0.0.0", port=8080)
+    #app.run(debug=True, port=8080)
